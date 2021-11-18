ad2fe20ef92d7701101e32a0b9ccdd7fb7ecaf99
saleborg <Stefan.aleborg@gmail.com>
2021-11-18 16:49:22 +0100

    First commit of e-com project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+import ProductList from "./components/ProductList";
+import Default from "./components/Default";
+import Cart from "./components/Cart";
+import Details from "./components/Details";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar />
+
+      <Routes>
+        <Route exact path="/" element={<ProductList />} />
+        <Route exact path="/details" element={<Details />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route path="*" element={<Default />} />
+      </Routes>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Default.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Default() {
+  return (
+    <div>
+      <h3>Hello from Default</h3>
+    </div>
+  );
+}
+
+export default Default;
